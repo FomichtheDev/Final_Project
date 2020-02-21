c6b5c93428fa90771daad48d7c1b9d89eff8a773
FomichtheDev <fomichg@gmail.com>
2020-02-21 17:39:51 +0200

    Adaptive

--- js/main.js
+++ js/main.js
@@ -1,19 +1,27 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $('.owl-carousel').owlCarousel({
-        loop:true,
-        margin:0,
-        nav:true,
-        navText: [ '', '' ],
+        loop: true,
+        margin: 0,
+        nav: true,
+        navText: ['', ''],
 
-        responsive:{
-            0:{
-                items:2
+        responsive: {
+            0: {
+                items: 2
             },
 
-            1000:{
-                items:2
+            1000: {
+                items: 2
             }
         }
     });
 
+});
+$(document).ready(function () {
+    $(".nav").on("click", "a", function (event) {
+        event.preventDefault();
+        var id = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
 });
